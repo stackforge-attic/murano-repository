8cc04eaedefacd6ebc17b6cae34c6383a8f08012
Timur Sufiev <tsufiev@mirantis.com>
2013-10-25 13:38:25 +0400

    Improve server-side caching.
    Recreate cache when files are changed via admin API.
    Send existing archive if data is hached.
    Also modify .gitignore and setup.cfg
    
    Change-Id: Ifb67856965586c2c5a115303e11c7b9856162aaf

--- muranorepository/api/v1.py
+++ muranorepository/api/v1.py
@@ -22,28 +22,57 @@ from werkzeug import secure_filename
 from muranorepository.utils.parser import ManifestParser
 from muranorepository.utils.archiver import Archiver
 from muranorepository.consts import DATA_TYPES, MANIFEST
-
+from muranorepository.consts import CLIENTS_DICT
+from muranorepository.consts import ARCHIVE_PKG_NAME
+import logging as log
 from oslo.config import cfg
 CONF = cfg.CONF
 
 v1_api = Blueprint('v1', __name__)
 CACHE_DIR = os.path.join(v1_api.root_path, 'cache')
-
 if not os.path.exists(CACHE_DIR):
     os.mkdir(CACHE_DIR)
+
+def _update_hash(data_type):
+    client = None
+    for client_type, client_data_types in CLIENTS_DICT.iteritems():
+        if data_type in client_data_types:
+            client = client_type
+            break
+    if not client:
+        abort(404)
+    cache_dir = os.path.join(CACHE_DIR, client)
+    if not os.path.exists(cache_dir):
+        return None
+    manifests = ManifestParser(CONF.manifests).parse()
+    archive_manager = Archiver()
+    existing_hash = archive_manager.get_existing_hash(cache_dir)
+    if not existing_hash:
+        archive_manager.remove_existing_hash(cache_dir, existing_hash)
+    else:
+        archive_manager.create(cache_dir, manifests, CLIENTS_DICT[client])
 
 
 def _get_archive(client, hash_sum):
     parser = ManifestParser(CONF.manifests)
     manifests = parser.parse()
-    types = None
-    if client == 'conductor':
-        types = ('heat', 'agent', 'scripts', 'workflows')
-    elif client == 'ui':
-        types = ('ui',)
-    else:
+    types = CLIENTS_DICT.get(client)
+    if not types:
         abort(404)
-    return Archiver().create(client, CACHE_DIR, manifests, hash_sum, types)
+    archive_manager = Archiver()
+    cache_dir = os.path.join(CACHE_DIR, client)
+    if not os.path.exists(cache_dir):
+        os.mkdir(cache_dir)
+    existing_hash = archive_manager.get_existing_hash(cache_dir)
+
+    if existing_hash and hash_sum is None:
+        log.debug('Transfer existing archive')
+        return os.path.join(cache_dir, existing_hash, ARCHIVE_PKG_NAME)
+
+    if archive_manager._hashes_match(cache_dir, existing_hash, hash_sum):
+        return None
+
+    return archive_manager.create(cache_dir, manifests, types)
 
 
 def _get_locations(data_type, result_path):
@@ -64,11 +93,19 @@ def _get_locations(data_type, result_path):
     return jsonify({data_type: locations})
 
 
-def _save_file(request, result_path):
+def _save_file(request, data_type, path=None):
+    if path:
+        result_path = _compose_path(data_type, path)
+        #subfolder should already exists
+        if not os.path.exists(result_path):
+            abort(404)
+    else:
+        result_path = _compose_path(data_type)
     file_to_upload = request.files.get('file')
     if file_to_upload:
         filename = secure_filename(file_to_upload.filename)
         file_to_upload.save(os.path.join(result_path, filename))
+        _update_hash(data_type)
         return jsonify(result='success')
     else:
         abort(400)
@@ -105,9 +142,8 @@ def get_data_type_locations(data_type):
 @v1_api.route('/admin/<data_type>', methods=['POST'])
 def upload_file(data_type):
     _check_data_type(data_type)
-    result_path = _compose_path(data_type)
     try:
-        return _save_file(request, result_path)
+        return _save_file(request, data_type)
     except:
         abort(403)
 
@@ -125,13 +161,10 @@ def _get_locations_in_nested_path_or_get_file(data_type, path):
 @v1_api.route('/admin/<data_type>/<path:path>', methods=['POST'])
 def upload_file_in_nested_path(data_type, path):
     _check_data_type(data_type)
-    result_path = _compose_path(data_type, path)
-    # it's forbidden to upload manifests to subfolder
+    # it's forbidden to upload manifests to subfolders
     if data_type == MANIFEST:
         abort(403)
-    if not os.path.exists(result_path):
-        abort(404)
-    return _save_file(request, result_path)
+    return _save_file(request, data_type, path)
 
 
 @v1_api.route('/admin/<data_type>/<path:path>', methods=['PUT'])
@@ -159,22 +192,13 @@ def delete_directory_or_file(data_type, path):
     if os.path.isfile(result_path):
         try:
             os.remove(result_path)
+            _update_hash(data_type)
         except Exception:
             abort(404)
     else:
         try:
+            # enable to delete only empty directories
             os.rmdir(result_path)
         except Exception:
             abort(403)
     return jsonify(result='success')
-
-
-@v1_api.route('/admin/services', methods=['GET'])
-def get_services_list():
-    manifests = ManifestParser(CONF.manifests).parse()
-    excluded_fields = set(DATA_TYPES) - set(MANIFEST)
-    data = []
-    for manifest in manifests:
-        data.append(dict((k, v) for k, v in manifest.__dict__.iteritems()
-                         if not k in excluded_fields))
-    return jsonify(tuple(data))
