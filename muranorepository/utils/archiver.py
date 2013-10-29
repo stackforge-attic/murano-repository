8c1b86603aae2316fe03804b49e8d15a8f454197
Ekaterina Fedorova <efedorova@mirantis.com>
2013-10-29 19:15:01 +0400

    Add new api call: download service archive
    Add api call: Get service files list by data_type
    Update Archiver to configure destination folder from config or by key
    
    Change-Id: Ie457825cf6d6a250581d121b23a19633bc2f2d58

--- muranorepository/utils/archiver.py
+++ muranorepository/utils/archiver.py
@@ -25,7 +25,26 @@ CHUNK_SIZE = 1 << 20  # 1MB
 
 
 class Archiver(object):
-    def _copy_data(self, file_lists, src, dst):
+    def __init__(self, src_by_data_type=False, dst_by_data_type=False):
+        self.dst_directories = {}
+        self.src_directories = {}
+        if src_by_data_type:
+             # Use data_type key as destination folder
+            for data_type in DATA_TYPES:
+                self.src_directories[data_type] = data_type
+        else:
+            for data_type in DATA_TYPES:
+                self.src_directories[data_type] = getattr(CONF, data_type)
+
+        if dst_by_data_type:
+            for data_type in DATA_TYPES:
+                self.dst_directories[data_type] = data_type
+        else:
+            for data_type in DATA_TYPES:
+                self.dst_directories[data_type] = getattr(CONF.output,
+                                                          data_type)
+
+    def _copy_data(self, file_lists, src, dst, overwrite=True):
         if not os.path.exists(dst):
             os.makedirs(dst)
 
@@ -36,11 +55,12 @@ class Archiver(object):
 
             if (base_dir != dst) and (not os.path.exists(base_dir)):
                 os.makedirs(os.path.dirname(destination))
+            if os.path.exists(destination) and not overwrite:
+                raise IOError('File {0} already exists'.format(destination))
             try:
-                shutil.copyfile(source, destination)
+                shutil.copy(source, destination)
             except IOError:
-                log.error("Unable to copy file "
-                          "{0}".format(file))
+                log.error('Unable to copy file {0}'.format(file))
 
     def _get_hash(self, archive_path):
         """Calculate SHA1-hash of archive file.
@@ -57,33 +77,51 @@ class Archiver(object):
                     sha1.update(buf)
                     buf = f.read(CHUNK_SIZE)
             hsum = sha1.hexdigest()
-            log.debug("Archive '{0}' has hash-sum {1}".format(
-                archive_path, hsum))
+            log.debug('Archive {0} has hash-sum {1}'.format(archive_path,
+                                                            hsum))
             return hsum
         else:
             log.info(
-                "Archive '{0}' doesn't exist, no hash to calculate".format(
+                'Archive {0} does not exist, no hash to calculate'.format(
                     archive_path))
             return None
 
-    def _compose_archive(self, path, cache_dir):
-        with tarfile.open(ARCHIVE_PKG_NAME, "w:gz") as tar:
+    def _compose_archive(self, arch_name, path, hash=False, cache_dir=None):
+        with tarfile.open(arch_name, 'w:gz') as tar:
             for item in os.listdir(path):
                 tar.add(os.path.join(path, item), item)
         try:
             shutil.rmtree(path, ignore_errors=True)
         except Exception as e:
-            log.error("Unable to delete temp directory: {0}".format(e))
-        hash_folder = self._create_hash_folder(ARCHIVE_PKG_NAME, cache_dir)
-        try:
-            shutil.move(ARCHIVE_PKG_NAME, os.path.join(hash_folder,
-                                                       ARCHIVE_PKG_NAME))
-        except Exception as e:
-            log.error('Unable to move created archive {0}'
-                      ' to hash folder {1} due to {2}'.format(ARCHIVE_PKG_NAME,
-                                                              hash_folder,
-                                                              e))
-        return os.path.abspath(os.path.join(hash_folder, ARCHIVE_PKG_NAME))
+            log.error('Unable to delete temp directory: {0}'.format(e))
+        if not hash:
+            return arch_name
+        else:
+            if not cache_dir:
+                raise ValueError('cache_dir parameter should not be None. '
+                                 'It is needed to create hash directory')
+            hash_folder = self._create_hash_folder(arch_name, cache_dir)
+            try:
+                shutil.move(ARCHIVE_PKG_NAME, os.path.join(hash_folder,
+                                                           arch_name))
+            except Exception as e:
+                log.error('Unable to move created archive {0}'
+                          ' to hash folder {1} due to {2}'.format(arch_name,
+                                                                  hash_folder,
+                                                                  e))
+            return os.path.abspath(os.path.join(hash_folder, arch_name))
+
+    def _create_hash_folder(self, archive_name, cache_dir):
+        """
+        Creates folder with data archive inside that has
+        name equals to hash calculated from archive
+        Return path to created hash folder
+        """
+        hash_sum = self._get_hash(archive_name)
+        pkg_dir = os.path.join(cache_dir, hash_sum)
+        if not os.path.exists(pkg_dir):
+            os.mkdir(pkg_dir)
+        return pkg_dir
 
     def get_existing_hash(self, cache_dir):
         existing_caches = os.listdir(cache_dir)
@@ -115,6 +153,7 @@ class Archiver(object):
 
     def create(self, cache_dir, manifests, types):
         """
+        cache_dir - full path to dir where cache contains
         manifests -- list of Manifest objects
         types -- desired data types to be added to archive
         return: absolute path to created archive
@@ -126,8 +165,9 @@ class Archiver(object):
             temp_dir = '/tmp'
         for data_type in types:
             if data_type not in DATA_TYPES:
-                raise Exception("Please, specify one of the supported data "
-                                "types: {0}".format(DATA_TYPES))
+                raise Exception(' {0} data type specified for archiving is not'
+                                ' valid. Supported data types are: '
+                                '{1}'.format(data_type, DATA_TYPES))
 
             for manifest in manifests:
                 if not manifest.enabled and not manifest.valid:
@@ -135,32 +175,37 @@ class Archiver(object):
 
                 if hasattr(manifest, data_type):
                     file_list = getattr(manifest, data_type)
-                    dst_directory = os.path.join(temp_dir,
-                                                 getattr(CONF.output,
-                                                         data_type))
-                    scr_directory = os.path.join(CONF.manifests,
-                                                 getattr(CONF, data_type))
+                    scr_directory = os.path.join(
+                        CONF.manifests, self.src_directories[data_type])
+                    dst_directory = os.path.join(
+                        temp_dir, self.dst_directories[data_type])
                     self._copy_data(file_list, scr_directory, dst_directory)
                 else:
                     log.info(
-                        "Manifest for {0} service has no file definitions for "
-                        "{1}".format(manifest.service_display_name, data_type))
+                        'Manifest for {0} service has no file definitions for '
+                        '{1}'.format(manifest.service_display_name, data_type))
+        return self._compose_archive(ARCHIVE_PKG_NAME,
+                                     temp_dir,
+                                     hash=True,
+                                     cache_dir=cache_dir)
 
-        return self._compose_archive(temp_dir, cache_dir)
+    def create_service_archive(self, manifest, file_name):
+        temp_dir = tempfile.mkdtemp()
+        for data_type in DATA_TYPES:
+            if hasattr(manifest, data_type):
+                file_list = getattr(manifest, data_type)
+                scr_directory = os.path.join(
+                    CONF.manifests, self.src_directories[data_type])
+                dst_directory = os.path.join(
+                    temp_dir, self.dst_directories[data_type])
+                self._copy_data(file_list, scr_directory, dst_directory)
+            else:
+                log.info(
+                    '{0} manifest has no file definitions for '
+                    '{1}'.format(manifest.service_display_name, data_type))
+        return self._compose_archive(file_name, temp_dir)
 
     def remove_existing_hash(self, cache_dir, hash):
         path = os.path.join(cache_dir, hash)
         log.info('Deleting archive package from {0}.'.format(path))
         shutil.rmtree(path, ignore_errors=True)
-
-    def _create_hash_folder(self, archive_name, cache_dir):
-        """
-        Creates folder with data archive inside that has
-        name equals to hash calculated from archive
-        Return path to created hash folder
-        """
-        hash_sum = self._get_hash(archive_name)
-        pkg_dir = os.path.join(cache_dir, hash_sum)
-        if not os.path.exists(pkg_dir):
-            os.mkdir(pkg_dir)
-        return pkg_dir
