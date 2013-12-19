ebae6da01e08bce63282a543c7dfd5434cf299f6
Igor Yozhikov <iyozhikov@mirantis.com>
2013-12-19 17:51:00 +0400

    Made changes for nonroot daemons run
    Change-Id: I1c2186e2340d429a75ef3bcedb3ba73a3060fdb2

--- setup.sh
+++ setup.sh
@@ -23,6 +23,7 @@ GIT_CLONE_DIR=`echo $SERVICE_CONTENT_DIRECTORY | sed -e "s/$SERVICE_SRV_NAME//"`
 #ETC_CFG_DIR="/etc/$SERVICE_SRV_NAME"
 ETC_CFG_DIR="/etc/murano"
 LOG_DIR="/var/log/murano/"
+CACHE_DIR="/var/cache/murano/"
 SERVICE_CONFIG_FILE_PATH="$ETC_CFG_DIR/murano-repository.conf"
 
 # Functions
@@ -147,6 +148,14 @@ CLONE_FROM_GIT=$1
 	do
 		cp -f "$SERVICE_CONTENT_DIRECTORY/etc/murano/$file" "$ETC_CFG_DIR/$file"
 	done
+        log "Making common $CACHE_DIR"
+        if [ ! -d "$CACHE_DIR" ]; then
+                mkdir -p $CACHE_DIR
+                if [ $? -ne 0 ];then
+                        log "Can't create \"$CACHE_DIR\", exiting!!!"
+                        exit 1
+                fi
+        fi
 }
 
 # searching for service executable in path
