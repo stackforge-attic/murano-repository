c4b58fcc004f55364afd6e4a6438262747a5dd5a
Ekaterina Fedorova <efedorova@mirantis.com>
2014-01-15 16:39:54 +0400

    Return correct http code
    During toggle enabled 500 was sent in case service is not defined
    Fix return code to 404
    Closes-Bug: #1268976
    Change-Id: I726d8b8849b2ccd44fdbf9d7b4ce65f3a62cf4fc

--- muranorepository/utils/parser.py
+++ muranorepository/utils/parser.py
@@ -145,20 +145,28 @@ class ManifestParser(object):
                             '{0}-manifest.yaml'.format(service_name))
 
     def toggle_enabled(self, service_name):
+        """
+        :param service_name: string, name of the service
+        :return: Status code with operation result
+        """
         path_to_manifest = self._get_manifest_path(service_name)
-
-        if not path_to_manifest:
+        if not os.path.exists(path_to_manifest):
             log.error(_("There is no manifest "
                         "file for '{0}' service".format(service_name)))
-            return False
-        with open(path_to_manifest) as stream:
-            service_manifest_data = yaml.load(stream)
-        service_manifest_data['enabled'] = \
-            not service_manifest_data.get('enabled')
-        with open(path_to_manifest, 'w') as manifest_file:
-            manifest_file.write(yaml.dump(service_manifest_data,
-                                          default_flow_style=False))
-        return True
+            return 404
+        try:
+            with open(path_to_manifest) as stream:
+                service_manifest_data = yaml.load(stream)
+            service_manifest_data['enabled'] = \
+                not service_manifest_data.get('enabled')
+            with open(path_to_manifest, 'w') as manifest_file:
+                manifest_file.write(yaml.dump(service_manifest_data,
+                                              default_flow_style=False))
+        except IOError:
+            log.exception(_('Error during toggle enabled operation'))
+            return 505
+
+        return 200
 
     def update_service(self, service_name, data):
         path_to_manifest = self._get_manifest_path(service_name)
